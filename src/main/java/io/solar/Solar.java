541be4d4761816e8fc7f294039b07a21157c5b2d
vitekes <vitekes [at] gmail.com>
2021-04-20 23:35:22 +0300

    Added initial files

--- src/main/java/io/solar/Solar.java
+++ src/main/java/io/solar/Solar.java
@@ -1,7 +1,9 @@
 package io.solar;
 
+import java.util.Arrays;
+
 public class Solar {
     public static void main(String[] args) {
-
+        System.out.println("args = " + Arrays.deepToString(args));
     }
 }
